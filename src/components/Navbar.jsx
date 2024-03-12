447d9f45b686d2a673baf313ea9dd882929bd0a5
blaztted <bbc.silva@gmail.com>
2024-03-12 16:20:46 +0000

    created navbar component and added bootstrap template navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+function NavBar() {
+  const [scrolling, setScrolling] = useState(false);
+
+  // When the user scrolls down, the navbar have some shadow
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <Navbar
+      bg="white"
+      data-bs-theme="light"
+      expand="lg"
+      fixed="top"
+      className={scrolling ? "shadow-sm" : ""}
+    >
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
